5bbd5530a2ae60ea9cda2d73a64b29e2425562e2
Mexriddin <maxkamtayev@gmail.com>
2023-12-10 17:38:30 +0500

    add test 'go to login from product page' in product_page

--- test_product_page.py
+++ test_product_page.py
@@ -18,6 +18,20 @@ def test_guest_can_add_product_to_basket(browser, promo):
     product_page.should_be_price_in_message_add_basket()
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.go_to_login_page()
+
+
 @pytest.mark.xfail(reason="Not finished it's a feature")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
