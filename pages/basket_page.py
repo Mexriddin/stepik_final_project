34f4e356419392f13c749f080b617e5b3d7080d4
Mexriddin <maxkamtayev@gmail.com>
2023-12-10 18:19:09 +0500

    change assert for  basket_page tests

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -9,5 +9,5 @@ class BasketPage(BasePage):
             "Products item  is resented, but should not be"
 
     def should_be_text_empty_basket(self):
-        assert 'empty' in self.browser.find_element(*BasketPageLocators.TEXT_THAT_EMPTY).text, \
-            "Text indicating that the cart is empty is not available"
+        assert self.is_element_present(*BasketPageLocators.TEXT_THAT_EMPTY), \
+            "Text indicating that the cart is empty is not present"
