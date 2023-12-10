3024e073d164184cb03ecfced84b768f1cf9cb2f
Mexriddin <maxkamtayev@gmail.com>
2023-12-10 18:44:25 +0500

    add marks

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,22 @@
-from pages.basket_page import BasketPage
-from pages.login_page import LoginPage
+import pytest
+
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_should_see_login_link_on_main_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        main_page = MainPage(browser=browser, url=link)
+        main_page.open()
+        main_page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_main_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.go_to_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,23 @@
 import pytest
-
-from pages.basket_page import BasketPage
+from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
+
+
+@pytest.mark.login
+class TestLoginFromProductPage():
+    def test_guest_should_see_login_link_on_product_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_product_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.go_to_login_page()
+
+
 
 promos = ['offer0', 'offer1', 'offer2', 'offer3', 'offer4', 'offer5', 'offer6',
           pytest.param("offer5", marks=pytest.mark.xfail),
@@ -18,20 +34,6 @@ def test_guest_can_add_product_to_basket(browser, promo):
     product_page.should_be_message_add_to_basket()
     product_page.should_be_name_in_message_add_basket()
     product_page.should_be_price_in_message_add_basket()
-
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_login_link()
-
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.go_to_login_page()
 
 
 @pytest.mark.xfail(reason="Not finished it's a feature")
